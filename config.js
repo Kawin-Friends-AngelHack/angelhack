bea35dc77202782d399be6ee64121b5a17ef3ed8
Kawin Tiraborisue <time@finnomena.com>
2018-09-02 00:28:46 +0700

    fix for heroku

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    mysql:{
+        host: '35.207.43.138',
+        user: 'admin',
+        password: 'nv932vr3DFS!',
+        database: 'timeff-web-db',
+        connectionLimit: 10
+    }
+}
